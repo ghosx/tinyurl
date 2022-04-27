d3be98dc2f7a03fd343b2f4017773b4972a46b5f
ghosx <ghosx@qq.com>
2022-04-27 19:19:21 +0800

    add set_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,14 @@
+cd /tmp
+wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" > /etc/profile
+echo "export PATH=$PATH:$HOME/go/bin" >> /etc/profile
+echo "export GO111MODULE=on" >> /etc/profile
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2
+go install google.golang.org/protobuf/cmd/protoc-gen-go
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+
+
